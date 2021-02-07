c15f1b024aac37622b74a37a55e58fc123ccc6fe
kagera <52308117+kagerka@users.noreply.github.com>
2021-02-07 22:33:44 +0300

    feat: add function that returns turns and seconds

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  let result = {
+    turns: 0,
+    seconds: 0,
+  }
+  let turnsCount = 1;
+  while (disksNumber > 1) {
+    turnsCount = (turnsCount * 2) + 1;
+    disksNumber--;
+  }
+  let secondsCount = Math.floor(turnsCount * 3600 / turnsSpeed);
+  result.turns = turnsCount;
+  result.seconds = secondsCount;
+  return result;
 };
