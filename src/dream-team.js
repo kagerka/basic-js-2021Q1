b60787c91d5130bfc914347ac6b5cc4d27b7ca4e
kagera <52308117+kagerka@users.noreply.github.com>
2021-02-07 16:05:40 +0300

    feat: add function that returns name of team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let result = members.filter(el => typeof el === 'string');
+  result = result.map(el => el.trim()).join(', ').toUpperCase().split(', ');
+  result = result.map(el => el[0]).sort().join('');
+  return result;
 };
