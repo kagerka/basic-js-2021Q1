2a714cc21f10349678698aa409c52e32c7952276
kagera <52308117+kagerka@users.noreply.github.com>
2021-02-17 17:14:44 +0300

    feat: add solution for transform array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,47 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let newArr = arr.slice();
+  if (!Array.isArray(newArr)) {
+    throw new Error('Error');
+  }
+  if (newArr.length === 0 || newArr === 'Infinity') {
+    return [];
+  }
+  if (newArr.findIndex(el => el === '--discard-next' || el === '--discard-prev' || el === '--double-prev' || el === '--double-next') === -1) {
+    return newArr;
+  }
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i] == '--double-prev' && i == 0) {
+      newArr[i] = 'del';
+    }
+    if (newArr[i] == '--double-next' && i == newArr.length - 1) {
+      newArr[i] = 'del';
+    }
+    if (newArr[i] == '--discard-next' && i == newArr.length - 1) {
+      newArr[i] = 'del';
+    }
+    if (newArr[i] == '--discard-prev' && i == 0) {
+      newArr[i] = 'del';
+    }
+  }
+  for (let j = 0; j < newArr.length; j++) {
+    if (newArr[j] == '--discard-next') {
+      newArr[j + 1] = 'del';
+      newArr[j] = 'del';
+    }
+    if (newArr[j] == '--discard-prev') {
+      newArr[j - 1] = 'del';
+      newArr[j] = 'del';
+    }
+    if (newArr[j] == '--double-next') {
+      newArr[j] = newArr[j + 1];
+    }
+    if (newArr[j] == '--double-prev') {
+      newArr[j] = newArr[j - 1];
+    }
+  }
+  let result = newArr.filter(el => el !== 'del');
+  newArr = [];
+  return result;
 };
